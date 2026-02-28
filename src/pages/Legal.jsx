fe4be9b712197fd1b8168995876c05a2ea3edcdb
asif <asifarbaj@gmail.com>
2026-02-28 22:04:28 +0530

    feat: show only one registration number for 12A & 80G card

--- src/pages/Legal.jsx
+++ src/pages/Legal.jsx
@@ -13,23 +13,17 @@ const Legal = () => {
       description: "Registration with the Inspector General of Registration (IGR), Odisha for legal identity.",
       file: "/reports/registration-igr.pdf"
     },
-    {
+    /* {
       title: "District Registration",
       number: organizationInfo.registrations.districtNumber,
       description: "Primary registration as a society in Dhenkanal district of Odisha.",
       file: "/reports/registration-dkl.pdf"
-    },
+    }, */
     {
-      title: "Registered under 12A",
+      title: "Registered under 12A & 80G",
       number: organizationInfo.registrations.urn12A,
-      description: "Income Tax exemption certificate under section 12A of the Income Tax Act.",
+      description: "Income Tax exemption certificates under section 12A & 80G ensuring transparency and tax benefits for donors.",
       file: "/reports/12a.pdf"
-    },
-    {
-      title: "Registered under 80G",
-      number: organizationInfo.registrations.urn80G,
-      description: "Income Tax exemption certificate under section 80G, allowing tax benefits for donors.",
-      file: "/reports/80g.pdf"
     },
     {
       title: "Registered with CSR-1",
@@ -37,7 +31,7 @@ const Legal = () => {
       description: "Ministry of Corporate Affairs (MCA) registration for undertaking Corporate Social Responsibility projects.",
       file: "/reports/csr-1.pdf"
     },
-    {
+    /* {
       title: "PAN Card",
       number: organizationInfo.registrations.pan,
       description: "Permanent Account Number issued by the Income Tax Department of India.",
@@ -48,7 +42,7 @@ const Legal = () => {
       number: organizationInfo.registrations.gst,
       description: "Goods and Services Tax registration for statutory financial compliance.",
       file: "/reports/gst.pdf"
-    },
+    }, */
     {
       title: "Registered as VTP",
       number: organizationInfo.registrations.vtp,
